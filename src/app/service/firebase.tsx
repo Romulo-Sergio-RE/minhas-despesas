83a742b8a86f7d70999f08086a62ee249fc1e372
Romulo-Sergio-RE <89258510+Romulo-Sergio-RE@users.noreply.github.com>
2022-09-29 16:56:17 -0300

    pequena correcao

--- src/app/service/firebase.tsx
+++ src/app/service/firebase.tsx
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD7E-PxP1JOd-VKBq5U2xRRror4xcLFopo",
-    authDomain: "primeriocrudreact.firebaseapp.com",
-    databaseURL: "https://primeriocrudreact-default-rtdb.firebaseio.com",
-    projectId: "primeriocrudreact",
-    storageBucket: "primeriocrudreact.appspot.com",
-    messagingSenderId: "473328973090",
-    appId: "1:473328973090:web:b66d82ac4ec5bc2b9c617b",
-    measurementId: "G-MZ4F8QZ6LD"
+
 };
 
 // Initialize Firebase
