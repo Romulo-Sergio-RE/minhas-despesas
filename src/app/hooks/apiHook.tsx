3d4b73fce9bf44474f396ac10a66299f7e71f51d
Romulo-Sergio-RE <89258510+Romulo-Sergio-RE@users.noreply.github.com>
2022-10-20 16:37:36 -0300

    finalizando o hook

--- src/app/hooks/apiHook.tsx
+++ src/app/hooks/apiHook.tsx
@@ -1,16 +1,34 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http:/localhost:5000"
+    baseURL: "http://localhost:3000/"
 })
 
 export const useApi = ()=> ({
     getAllExpense: async ()=>{
-        const response = await api.get("/expense")
+        const response = await api.get("expense")
         return response.data
     },
     getExpenseById: async (id:string)=>{
-        const response = await api.get(`/expense/${id}`)
+        const response = await api.get(`expense/${id}`)
+        return response.data
+    },
+    postExpense: async (title: string, value:string)=>{
+        const response = await api.post("expense",{
+            "title":title,
+            "value":value,
+        })
+        return response.data
+    },
+    updateExpense: async (id:string, title:string, value:string)=>{
+        const response = await api.put(`expense/${id}`,{
+            "title":title,
+            "value":value,
+        })
+        return response.data
+    },
+    deleteExpense: async (id:string)=>{
+        const response = await api.delete(`expense/${id}`)
         return response.data
     }
 })
--- src/app/page/HomePage/index.tsx
+++ src/app/page/HomePage/index.tsx
@@ -7,13 +7,30 @@ import { useApi } from "../../hooks/apiHook"
 import { Container } from "./styled"
 
 export const HomePage = () =>{
-    const[espense, setExpense] = useState()
+    const[expense, setExpense] = useState([])
     const data = useApi()
 
-    const testeApi = async() =>{
+    const testeApi = async () =>{
         const dados = await data.getAllExpense()
         setExpense(dados)
     }
+    // const testeApi2 = async () =>{
+    //     const dados = await data.getExpenseById("634eaba1820102b7ac655b45")
+    //     setExpense(dados)
+    // }
+    // const testeApi2 = async () =>{
+    //     const dados = await data.postExpense("carro","350")
+    //     setExpense(dados)
+    // }
+    // const testeApi4 = async () =>{
+    //     const dados = await data.deleteExpense("634eaba1820102b7ac655b45")
+    //     setExpense(dados)
+    // }
+    // const testeApi5 = async () =>{
+    //     const dados = await data.updateExpense("634f13a154708f9a2d9e8e47","internet","250")
+    //     setExpense(dados)
+    // }
+    console.log(expense)
     return(
         <Container>
             <Header />
